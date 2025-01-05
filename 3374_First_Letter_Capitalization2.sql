26aa4b9ce16bbd3f0dacc7b4e9242d06a8467f2f
AngadSingh04 <singh04angad@gmail.com>
2025-01-05 09:43:03 +0530

    3374 First letter capitalization 2

---
+++ 3374_First_Letter_Capitalization2.sql
@@ -0,0 +1,8 @@
+-- Write your PostgreSQL query statement below
+select
+    content_id,
+    content_text as original_text,
+    initcap(content_text) as converted_text
+from user_content;
+
+-- learnt about initcap
