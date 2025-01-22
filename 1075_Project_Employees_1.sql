99fc22321b7be1ea8a3839dd1e2b98f64827020c
AngadSingh04 <singh04angad@gmail.com>
2025-01-22 07:33:00 +0530

    1075 Project Employees 1

--- 1075_Project_Employees_1.sql
+++ 1075_Project_Employees_1.sql
@@ -1,8 +1,8 @@
 -- Write your PostgreSQL query statement below
-with new_table as (select *
+select
+    project_id,
+    round(avg(experience_years),2) as average_years
 from project
 inner join employee
-on project.employee_id = employee.employee_id)
-select project_id, round(avg(experience_years),2) as average_years
-from new_table
+on project.employee_id=employee.employee_id
 group by project_id;
