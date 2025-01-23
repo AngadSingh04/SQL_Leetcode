0b269feb28fa97781127fd25f6218abbcc23358c
AngadSingh04 <singh04angad@gmail.com>
2025-01-23 07:39:58 +0530

    1633 Percentage of users attended a contest

--- 1633_Percentage_of_users.sql
+++ 1633_Percentage_of_users.sql
@@ -1,9 +1,9 @@
 -- Write your PostgreSQL query statement below
-select
+select 
     contest_id,
-    round(count(distinct register.user_id) * 100.0/(select count(*) from users),2) as percentage
+    round(count(distinct(register.user_id)) * 100.0/(select count(*) from users),2) as percentage
 from register
 inner join users
 on register.user_id=users.user_id
-group by contest_id 
+group by contest_id
 order by percentage desc, contest_id;
