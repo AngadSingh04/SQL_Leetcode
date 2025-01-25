58b46d5c80a3b3884966bbc6a6ddb95d0935e924
AngadSingh04 <singh04angad@gmail.com>
2025-01-25 07:32:13 +0530

    1978 employees whoose manager left the company

--- 1978_Employees_Manager.sql
+++ 1978_Employees_Manager.sql
@@ -1,6 +1,12 @@
 -- Write your PostgreSQL query statement below
-select employee_id from employees
+select 
+    employee_id
+from employees
 where manager_id not in (
-select employee_id
+select
+    employee_id
 from employees)
-and salary < 30000 order by employee_id
+and salary < 30000
+order by employee_id;
+
+-- Join method is better in terms of runtime
