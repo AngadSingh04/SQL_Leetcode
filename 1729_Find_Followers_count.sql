c17caf4f5ff6c78f31c8183f3a713e9f4f394adb
AngadSingh04 <singh04angad@gmail.com>
2025-02-01 20:59:18 +0530

    619 Biggest Single Number

--- 1729_Find_Followers_count.sql
+++ 1729_Find_Followers_count.sql
@@ -3,4 +3,4 @@ select
     user_id,
     count(distinct follower_id) as followers_count
 from followers
-group by user_id;
+group by user_id;619
---
+++ 619_Biggest_Single_Number.sql
@@ -0,0 +1,11 @@
+-- Write your PostgreSQL query statement below
+with cte as (select
+    num
+from mynumbers
+group by num
+having count(*)=1)
+
+select
+    max(num) as num
+from cte
+
