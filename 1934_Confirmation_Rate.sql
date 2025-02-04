d15d8b6096622236b8182e9d537f526dbb8a8e1d
AngadSingh04 <singh04angad@gmail.com>
2025-02-04 09:35:39 +0530

    1934 Confirmation Rate

--- 1934_Confirmation_Rate.sql
+++ 1934_Confirmation_Rate.sql
@@ -1,11 +1,9 @@
 -- Write your PostgreSQL query statement below
-select signups.user_id,
-round(case
-    when count(*) = 0 then 0
-    else count(case when action = 'confirmed' then 1 end) * 1.0/
-         count(*)
-end,2) as confirmation_rate
-from confirmations
-right outer join signups
-on confirmations.user_id = signups.user_id
-group by signups.user_id;
+select
+    signups.user_id,
+    case when count(*)=0 then 0 else
+    count(case when action = 'confirmed' then 1 end) * 1.0/count(*) end as confirmation_rate
+from signups
+left join confirmations
+on signups.user_id=confirmations.user_id
+group by signups.user_id
