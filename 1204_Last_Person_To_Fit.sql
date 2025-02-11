3fd887c405bac005091ba1af4218b1022a951439
AngadSingh04 <singh04angad@gmail.com>
2025-02-11 22:49:32 +0530

    1204 Last person to fit in the bus

--- 1204_Last_Person_To_Fit.sql
+++ 1204_Last_Person_To_Fit.sql
@@ -1,11 +1,11 @@
--- Write your PostgreSQL query statement below
-select person_name
-from (
-select 
+# Write your MySQL query statement below
+with cte as (select 
     *,
-    sum(weight) over(order by turn) as total_sum
-from queue
-)
-where total_sum <= 1000
-order by turn desc
+    sum(weight) over(order by turn) as sum_of_weights
+from queue)
+select
+    person_name
+from cte
+where sum_of_weights <= 1000
+order by person_name desc
 limit 1;
