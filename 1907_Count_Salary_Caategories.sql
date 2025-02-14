84cb7fbf6eb64525803b25c4fab8406f2b223ee9
AngadSingh04 <singh04angad@gmail.com>
2025-02-14 12:28:40 +0530

    1907 Count Salary Categories

--- 1907_Count_Salary_Caategories.sql
+++ 1907_Count_Salary_Caategories.sql
@@ -1,26 +1,18 @@
--- Write your PostgreSQL query statement below
-with cte as (select 
-    account_id,
+# Write your MySQL query statement below
+with cte as (select
     case
         when income < 20000 then 'Low Salary'
         when income between 20000 and 50000 then 'Average Salary'
-        else 'High Salary'
+        when income > 50000 then 'High Salary'
     end as category
 from accounts)
 
-select 
-    t2.category,
+select
+    category1.category,
     count(cte.category) as accounts_count
 from cte
-right outer join
-(select
-    'Low Salary' as category
-union all
-select 'Average Salary' as category
-union all
-select 'High Salary' as category) as t2
-on t2.category = cte.category
-group by t2.category;
-
-
-
+right outer join 
+(select 'Low Salary' as category union select 'High Salary' as category union select 'Average Salary') as category1
+on cte.category = category1.category
+group by category;
+    
